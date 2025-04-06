1397a3dfc33014ceefc0eea10270ffbeea411822
sotiriskaragiannis <skaragian03@gmail.com>
2025-04-06 17:42:57 +0300

    added problem 577

---
+++ 577.sql
@@ -0,0 +1,6 @@
+-- https://leetcode.com/problems/employee-bonus/description/
+
+SELECT name, bonus
+FROM Employee LEFT OUTER JOIN Bonus
+ON Employee.empId=Bonus.empId
+WHERE bonus < 1000 OR bonus IS NULL;
