7d155c8327ea6fada5f74a346f81a17231e45c2c
sotiriskaragiannis <skaragian03@gmail.com>
2025-04-06 17:29:25 +0300

    added problem 1661

---
+++ 1661.sql
@@ -0,0 +1,4 @@
+SELECT A1.machine_id, ROUND(SUM(A1.timestamp - A2.timestamp) / COUNT(A1.machine_id), 3) as processing_time
+FROM Activity A1 CROSS JOIN Activity A2
+ON A1.machine_id=A2.machine_id AND A1.process_id=A2.process_id AND A1.activity_type='end' AND A2.activity_type='start'
+GROUP BY A1.machine_id;
